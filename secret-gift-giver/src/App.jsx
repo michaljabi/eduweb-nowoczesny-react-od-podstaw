8bd8014670661e59318facd9844815044fe28d24
Jabi <michal.jabi@gmail.com>
2024-11-20 14:45:13 +0100

    lesson(10): map data to JSX elements

--- secret-gift-giver/src/App.jsx
+++ secret-gift-giver/src/App.jsx
@@ -1,16 +1,32 @@
 import {Header} from "./components/Header.jsx";
+import {people} from "./people.js";
 
 function App() {
 
   const appTitle = 'Secret Gift Giver';
 
   const showAlert = (message) => {
-        alert(message)
+      alert(message)
   }
 
   return (
     <>
-        <Header title={appTitle} subTitle="Exchange gifts with your colleagues" onAlert={showAlert} />
+      <Header title={appTitle} subTitle="Exchange gifts with your colleagues" onAlert={showAlert}/>
+
+        <main className="container mt-4 p-4">
+            <h3 className="title is-3">List of People</h3>
+            <article className="panel is-info">
+                {
+                    people.map(name =>
+                        (
+                            <a key={name} className="panel-block is-active">
+                                {name}
+                            </a>
+                        )
+                    )
+                }
+            </article>
+        </main>
     </>
   )
 }
