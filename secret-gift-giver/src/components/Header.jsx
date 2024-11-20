e304912421e12b215a6daa089930230074483b1e
Jabi <michal.jabi@gmail.com>
2024-11-20 14:24:58 +0100

    lesson(08): 5x JSX rules - how it behaves

--- secret-gift-giver/src/components/Header.jsx
+++ secret-gift-giver/src/components/Header.jsx
@@ -1,15 +1,31 @@
 
+function WhatWillBeRendered() {
+    return <>
+        { true }
+        { false && <p>!</p> }
+        { undefined } { null } { '' } { [] }
+        <hr />
+        { [1, 2, 3] } { <p>para</p> }
+    </>
+}
+
 export function Header() {
     return (
-        <section className="hero is-info">
-            <div className="hero-body">
-                <p className="title">Secret Gift Giver</p>
-                <p className="subtitle">Exchange gifts with your colleagues</p>
-            </div>
-        </section>
+        <>
+            <section className="hero is-info">
+                <div className="hero-body">
+                    <p className="title">Secret Gift Giver</p>
+                    <p className="subtitle p-1" style={ { backgroundColor: 'white', borderRadius: '5px' } }>Exchange gifts with your colleagues</p>
+                </div>
+            </section>
+            <WhatWillBeRendered />
+        </>
     )
 }
 
 // JSX rules:
 // 1. use className instead of class !
-// 2.
+// 2. always one root element in JSX !
+// 3. style is always an object !
+// 4. always use PascalCase for component names !
+// 5. some values are not rendered in JSX computation !
