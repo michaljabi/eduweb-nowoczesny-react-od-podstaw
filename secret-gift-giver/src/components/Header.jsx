4a7ec5729bc2d03a5f3406743db2274f52548554
Jabi <michal.jabi@gmail.com>
2024-11-20 13:48:36 +0100

    lesson(07): first component Header.jsx completed. bulma installed.

--- secret-gift-giver/src/components/Header.jsx
+++ secret-gift-giver/src/components/Header.jsx
@@ -1,4 +1,11 @@
 
 export function Header() {
-    return <></>
+    return (
+        <section className="hero is-info">
+            <div className="hero-body">
+                <p className="title">Secret Gift Giver</p>
+                <p className="subtitle">Exchange gifts with your colleagues</p>
+            </div>
+        </section>
+    )
 }
