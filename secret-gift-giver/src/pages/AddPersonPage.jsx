f5b2ed0f4b881b1ee54f8efdc28e26cb31f9e96c
Jabi <michal.jabi@gmail.com>
2024-11-20 18:15:16 +0100

    lesson(26): addPerson to in-memory people array via AddPersonPage form

--- secret-gift-giver/src/pages/AddPersonPage.jsx
+++ secret-gift-giver/src/pages/AddPersonPage.jsx
@@ -1,15 +1,29 @@
 import {PageLayout} from "../components/PageLayout.jsx";
+import {useState} from "react";
+import {addPerson} from "../people.js";
 
 export function AddPersonPage() {
+
+    const [personName, setPersonName] = useState("")
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        if(!personName) {
+            return;
+        }
+        addPerson(personName)
+        setPersonName("")
+    }
+
     return <PageLayout title="Add new person">
-        <form onSubmit={e => e.preventDefault()}>
+        <form onSubmit={handleSubmit}>
             <div className="field">
                 <p className="control">
-                    <input className="input" placeholder="Name"/>
+                    <input className="input" placeholder="Name" value={personName} onChange={(e) => setPersonName(e.target.value)} />
                 </p>
             </div>
             <div className="field">
-                <p className="control is-flex is-justify-content-end">
+                <p className="control is-flex is-justify-content-end" style={{ opacity: personName ? 1 : 0.6}}>
                     <button className="button is-info" type="submit">
                         Add
                     </button>
--- secret-gift-giver/src/people.js
+++ secret-gift-giver/src/people.js
@@ -1,1 +1,6 @@
 export const people = ["Eva", "Mark", "Krysia", "John", "Edmund"];
+
+
+export function addPerson(name) {
+    people.push(name);
+}
