7b52032a8d4d34672fdf084b0588542dc6cc4060
Jabi <michal.jabi@gmail.com>
2024-11-20 18:41:29 +0100

    lesson(30): div in PanelBlock already fixed :)

--- secret-gift-giver/src/components/PanelBlock.jsx
+++ secret-gift-giver/src/components/PanelBlock.jsx
@@ -12,5 +12,5 @@ export function PanelBlock({name = '', isSelected = false, onSelect = () => {}})
     return <div className={classNames("panel-block", {"is-active is-flex is-justify-content-space-between": isSelected})} onClick={onSelect}>
         <Link to={`/people/${name}`}> { name } </Link>
         { isSelected && <span>✅</span> }
-    </div>;
+    </div>
 }
